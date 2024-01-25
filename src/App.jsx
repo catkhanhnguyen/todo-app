dcdf7fc8b9af683373bfbf149c12e0b68134717c
Khanh Nguyen Le Cat <nlckhanh-batch43@sdc.edu.vn>
2024-01-25 14:33:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from '@mui/material'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Container sx={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'linear-gradient(to right, #ee9ca7, #ffdde1)'
+    }}>
+    
+    </Container>
+  )
+}
+
+export default App
